6ad50489efb55b1f714e9b78e8f2ecdf6fd51384
Anshuman Patel <bladorunnero@gmail.com>
2017-02-21 23:27:18 +0000

    Added plans table and many thing

--- db/migrate/20170207084644_create_contacts.rb
+++ db/migrate/20170207084644_create_contacts.rb
@@ -3,7 +3,9 @@ class CreateContacts < ActiveRecord::Migration[5.0]
     create_table :contacts do |t|
          t.string :name
          t.string :email
+         t.string :phone
          t.text :comments
+         
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170221091733) do
+ActiveRecord::Schema.define(version: 20170221231334) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
+    t.string   "phone"
     t.text     "comments"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "profiles", force: :cascade do |t|
+  create_table "plans", force: :cascade do |t|
+    t.string   "name"
+    t.decimal  "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
