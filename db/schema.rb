a0f5919bfbd64ba64affd7452c3d8490439b7096
Anshuman Patel <bladorunnero@gmail.com>
2017-02-21 09:01:51 +0000

    Added profiles table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170207084644) do
+ActiveRecord::Schema.define(version: 20170221090056) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
@@ -18,6 +18,9 @@ ActiveRecord::Schema.define(version: 20170207084644) do
     t.text     "comments"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "profiles", force: :cascade do |t|
   end
 
 end
